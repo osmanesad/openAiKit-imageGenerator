f8e7235b6ca05b2855bfd37acb67971e7200314e
osmanesad <byesad@gmail.com>
2022-12-14 22:52:10 +0300

    Update - Api Setup
    Gerekli paket kurulumunu yaptık. Api çalışması için gerekli temel kodları yazdık.

--- openAiKit-imageGenerator/ContentView.swift
+++ openAiKit-imageGenerator/ContentView.swift
@@ -6,6 +6,42 @@
 //
 
 import SwiftUI
+import OpenAIKit
+
+// MARK: - Kurulum - Setup
+// Buradaki key'i "https://beta.openai.com/account/api-keys" adrsinde üyeliğimizle giriş yaptıktan sonra ilgili bölümden kendimize bir api anahtarı oluşturabiliyoruz.
+
+final class ViewModal: ObservableObject{
+    private var openai: OpenAI?
+    func setup() {
+        openai = OpenAI(Configuration(
+            organization: "Personal",
+            apiKey: "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" // Buraya kenid api anahtarınızı girin.
+        ))
+    }
+    
+    func generateImage(prompt: String) async -> UIImage? {
+        guard let openai = openai else {
+            return nil
+        }
+        
+        do{
+            let params = ImageParameters(
+            prompt: prompt,
+            resolution: .medium,
+            responseFormat: .base64Json
+            )
+            let result = try await openai.createImage(parameters: params)
+            let data = result.data[0].image
+            let image = try openai.decodeBase64Image(data)
+            return image
+            
+        }catch {
+            print(String(describing: error))
+            return nil
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
