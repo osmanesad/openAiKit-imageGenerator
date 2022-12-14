a14cf0fc9addb63ee543325a120c1b44e09d53f2
osmanesad <byesad@gmail.com>
2022-12-14 22:27:15 +0300

    Initial Commit

---
+++ openAiKit-imageGenerator/openAiKit_imageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  openAiKit_imageGeneratorApp.swift
+//  openAiKit-imageGenerator
+//
+//  Created by Osman Esad on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct openAiKit_imageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
